7a24b7ff14d22ba86e68f2ac4ffedaae3a61b77f
19Mercy19 <107498653+19Mercy19@users.noreply.github.com>
2022-10-27 19:58:46 +0300

    Commit 2; Добавил все данные для одной карточки страницы

--- parser.py
+++ parser.py
@@ -7,30 +7,41 @@ url= 'https://www.list.am/category/56?pfreq=1&po=1&n=1&price1=&price2=&crc=-1&_a
 base_url = 'https://www.list.am/ru'
 
 
+
+
+# def get_products():
 response = requests.get(url, headers={'User-Agent': UserAgent().chrome})
-
 html = response.content
-
 soup = BeautifulSoup(html, 'lxml')
-
 products = soup.select_one('div.dl')
-
+# return products
 
 
 urls = []
+
 for product in products:
-    url = base_url + product.get('href')
-    urls.append(url)
+    try:
+        url = base_url + product.get('href')
+        urls.append(url)
+    except AttributeError:
+        continue
+print(len(urls))
 
-for url in urls:
-    response = requests.get(url, headers={'User-Agent': UserAgent().chrome})
-    html = response.content
-    soup = BeautifulSoup(html, 'lxml')
-    description = None
-    price = None
-    city = None
-    info = None
 
+# for url in urls:
+url = urls[0]
+response = requests.get(url, headers={'User-Agent': UserAgent().chrome})
+html = response.content
+soup = BeautifulSoup(html, 'lxml')
+info = soup.select_one('div.vit')
+image = info.select_one('div.pv').select_one('img').get('src')
+all_desc = info.select_one('div.vih')
+bar = all_desc.select_one('div#abar')
+price = bar.select_one('div.price').find('span', {'class': 'price x'}).text
+place = bar.select_one('div.loc').select_one('a').text
+desc = all_desc.select_one('h1').text
+
+print(url, desc, price, place, image, sep='\n')
 
 
 
